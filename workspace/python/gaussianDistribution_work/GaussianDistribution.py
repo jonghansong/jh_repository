ec6e0f5d980b29caf649fec0fe6a30ee2d783454
jonghansong <dev@macmini-roy.local>
2022-04-01 16:41:32 +0900

    token test commit

--- workspace/python/gaussianDistribution_work/GaussianDistribution.py
+++ workspace/python/gaussianDistribution_work/GaussianDistribution.py
@@ -20,10 +20,10 @@ def run(total_number_of_people, number_of_people_in_a_group):
     if(total_number_of_people < number_of_people_in_a_group) :
         sys.exit("[Error] total_number_of_people must be smaller than the number of people_in_a_group.")
 
-    
-
 def printError(msg) :
     print("[Error] : {}".format(msg))
 
 if __name__ == "__main__" :
     run();
+
+# test
