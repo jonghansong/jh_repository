6297276386fb70b5c8e6412b13f4d33617012f37
dev <dev@macmini-roy.local>
2022-04-01 16:13:20 +0900

    commit temp.

--- workspace/python/gaussianDistribution_work/GaussianDistribution.py
+++ workspace/python/gaussianDistribution_work/GaussianDistribution.py
@@ -1,19 +1,29 @@
+import sys
 import click
 #import yaml
 
 
-# get arguments for total number of people, number of people in a group.
+# get arguments for total number of people and number of people in a group.
 # generate expected vote result for yaml.
 # import yaml
 # draw plot.
+
+#@click.group()
+#def run():
+#    pass
 
 @click.command()
 @click.argument('total_number_of_people')
 @click.argument('number_of_people_in_a_group')
 def run(total_number_of_people, number_of_people_in_a_group):
-    print("$1, $2") #total_number_of_people, number_of_people_in_a_group)
-    print("Hello python")
+    print('total_number_of_people: {}, number_of_people_in_a_group: {}'.format(total_number_of_people, number_of_people_in_a_group))
+    if(total_number_of_people < number_of_people_in_a_group) :
+        sys.exit("[Error] total_number_of_people must be smaller than the number of people_in_a_group.")
 
+    
+
+def printError(msg) :
+    print("[Error] : {}".format(msg))
 
 if __name__ == "__main__" :
     run();
--- workspace/python/gaussianDistribution_work/run.sh
+++ workspace/python/gaussianDistribution_work/run.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 
-python -m venv venv
-./venv/Scripts/activate
+# remove venv directory
+if [ -e venv ]; then
+    rm -rf venv
+fi
+
+# create virtual environment
+python3 -m venv venv
+
+# activate virtual environment
+act_bin=`find ./venv -name "activate"`
+echo $act_bin
+source ${act_bin}
+if [ $? -ne 1 ]; then
+    echo "activate is fail."
+    #exit 1
+fi
+
+# install packages
+pip install click
+
+# run program
