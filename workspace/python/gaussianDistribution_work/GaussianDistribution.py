0b3183dc4a02b63b74f3de5db0bdccb39d4afc4d
jonghansong <sjonghan@naver.com>
2022-04-04 21:57:08 +0900

    added a function, runRandomSelect. dump yaml.

--- workspace/python/gaussianDistribution_work/GaussianDistribution.py
+++ workspace/python/gaussianDistribution_work/GaussianDistribution.py
@@ -1,7 +1,7 @@
 import sys
 import click
 import random
-
+import yaml
 
 # get arguments for total number of people and number of people in a group.
 # generate expected vote result for yaml.
@@ -23,23 +23,31 @@ def run(total, group):
     if(vTotal < vGroup) :
         sys.exit("[Error] total_number_of_people must be smaller than the number of people_in_a_group.")
 
+    out = {}
+    runRandomSelect(vTotal, vGroup, out)
 
-    repeat_cnt = vTotal / vGroup if vTotal % vGroup == 0 else vTotal / vGroup + 1
-    print(repeat_cnt)
+    with open('out.yaml', 'w') as f:
+        yaml.dump(out, f)
+
+    
+
+
+def runRandomSelect(total, group, out) :
+    repeat_cnt = total / group if total % group == 0 else total / group + 1
 
     for i in range(int(repeat_cnt)) :
         nSelectZero = 0
         nSelectOne = 0
-        for j in range(vGroup) :
+        for j in range(group) :
             v = random.randrange(0,2)
             assert(v == 0 or v == 1)
             if(v == 0) :
                 nSelectZero = nSelectZero + 1
             else :
                 nSelectOne = nSelectOne + 1
-        rZero = nSelectZero / vGroup
+        rZero = nSelectZero / group
         rOne = 1 - rZero
-        print("select zero : {}, select one : {}".format(rZero, rOne))
+        out[i] = {0:rZero, 1:rOne}
 
 
 def printError(msg) :
